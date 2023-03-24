051b35c8ebda1d6ae4490280d03be588653ae9e4
n21433312 <n21433312@gmail.com>
2023-03-25 02:30:18 +0900

    rooms admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -3,8 +3,41 @@ from .models import Room, Amenity
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+    
+    list_display = (
+        "name",
+        "price",
+        "kind",
+        "owner",
+    )
+
+    list_filter = (
+        "country",
+        "city",
+        "pet_friendly",
+        "kind",
+        "amenities",
+    )
 
 @admin.register(Amenity)
 class AmenityAdmin(admin.ModelAdmin):
-    pass
+    
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
+
+    list_filter = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
+
+    readonly_fields = ( #수정불가 필드
+        "created_at",
+        "updated_at",
+
+    )
