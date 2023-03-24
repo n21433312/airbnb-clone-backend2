0a9c7659718771e12da25bd2ddcde341cb8d5a1b
n21433312 <n21433312@gmail.com>
2023-03-25 00:34:39 +0900

    make User Model

--- users/admin.py
+++ users/admin.py
@@ -10,7 +10,18 @@ class CustomUserAdmin(UserAdmin):
         (
             "Profile",
             {
-                "fields": ("username", "password", "name", "email", "is_host"), "classes": ("wide",),
+                "fields": (
+                    "avatar", 
+                    "username", 
+                    "password", 
+                    "name", 
+                    "email", 
+                    "is_host", 
+                    "gender", 
+                    "language", 
+                    "currency", 
+                ), 
+                "classes": ("wide",),
             },
         ),
         ("Permissions",
