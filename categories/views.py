a651864bc131b988d5c4ae3c1f307ca7dadf86f8
n21433312 <n21433312@gmail.com>
2023-03-30 01:29:22 +0900

    유저에게 데이터 받아서 DB 수정하기

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
@@ -13,13 +14,30 @@ def categories(request):
     elif request.method == "POST":
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid(): #is_valid 유저가 보낸 데이터가 유효한지 검사하는기능
-            new_category = serializer.save()
+            new_category = serializer.save() #create 실행, 사용자 데이터만으로 serializer 만들기 때문
             return Response(CategorySerializer(new_category).data, ) 
         else:
             return Response(serializer.errors) 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data) 
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+    
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category, 
+            data=request.data, 
+            partial=True, #모든데이터 변경이아닌 일부만 변경 하고싶을때
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save() #create가 아닌 update실행, 사용자 데이터 + DB의 데이터로 serializer를 만들기 때문
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
