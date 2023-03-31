baf4c46e273856ed2af6a82c3b3a7a6d323e8e9c
n21433312 <n21433312@gmail.com>
2023-03-31 18:08:56 +0900

    Perk,Amenity get,post,put,delete 구현

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,8 +1,8 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from .models import Category
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(ModelSerializer):
 
     class Meta:
         model = Category
-        fields =  "__all__" #필드를 하나씩 보여주거나 몇몇개를 제외하거나 전부 보여줄수 있음.
+        fields =  "__all__" #필드를 하나씩[fields="name","kind",)] 보여주거나 몇몇개를 제외[exclude("created_at",)]하거나 전부[fields =  "__all__"] 보여줄수 있음.
--- categories/urls3.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.Categories.as_view()), #calss를 불러올려면 as_view를 써야함
-    path("<int:pk>", views.CategoryDetail.as_view())
-
-]
