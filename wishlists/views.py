1055056869f67f0b307d115c735857561c17dc45
n21433312 <n21433312@gmail.com>
2023-04-04 02:00:29 +0900

    whislist get, post 구현

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Wishlist
+from .serializers import WishlistSerializer
 
-# Create your views here.
+class Wishlists(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        all_wishlist = Wishlist.objects.filter(user=request.user)
+        serializer = WishlistSerializer(all_wishlist, many=True, context={"request": request})
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = WishlistSerializer(data=request.data)
+        if serializer.is_valid():
+           whishlist =  serializer.save(user=request.user, )
+           serializer = WishlistSerializer(whishlist)
+           return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
