587af54e49db5ce6a352638a9a047e5e2c83c496
n21433312 <n21433312@gmail.com>
2023-04-05 01:33:56 +0900

    Public 유저 정보 만들기, 비밀번호 변경기능 추가

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.exceptions import ParseError
+from rest_framework.exceptions import ParseError, NotFound
 from rest_framework.permissions import IsAuthenticated
-from . import serializers 
+from . import serializers, models
 
 class Me(APIView):
      
@@ -34,7 +34,38 @@ class Users(APIView):
         serializer = serializers.PrivateUserSerializer(data = request.data)
         if serializer.is_valid():
             user = serializer.save()
+            user.set_password(password)
+            user.save()
             serializer = serializers.PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+        
+
+class PublicUser(APIView):
+
+    def get(self, requset, username):
+        try:    
+            user = models.User.objects.get(username=username)
+        except models.User.DoesNotExist:
+            raise NotFound
+        serializer = serializers.PrivateUserSerializer(user)
+        return Response(serializer.data)
+    
+
+class ChangePassword(APIView):
+    
+    permission_classes = [IsAuthenticated]
+
+    def put(self, requset):
+        user = requset.user
+        old_password = requset.data.get("old_password")
+        new_password = requset.data.get("new_password")
+        if not old_password or not new_password:
+            raise ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password) #set_password는 new_password를 해쉬할 때만 작동한다, 따라서 uesr.save()가 필요
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
