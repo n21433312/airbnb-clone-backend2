ebd09447748e02638590c39dfa915d28c7662f30
n21433312 <n21433312@gmail.com>
2023-04-05 02:00:14 +0900

    로그인 로그아웃 추가

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -54,6 +55,7 @@ class PublicUser(APIView):
     
 
 class ChangePassword(APIView):
+
     
     permission_classes = [IsAuthenticated]
 
@@ -69,3 +71,31 @@ class ChangePassword(APIView):
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+
+class LogIn(APIView):
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(
+            request,
+            username = username,
+            password = password,
+        )
+        if user:
+            login(request, user)
+            return Response({"Ok":"Welcome!"})
+        else:
+            return Response({"error": "wrong password"})
+        
+
+class LogOut(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, requset):
+        logout(requset)
+        return Response({"Ok":"Bye"})
