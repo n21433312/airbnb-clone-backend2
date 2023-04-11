0231b131fda705251a6f3a9a3c55fe93b279b14d
n21433312 <n21433312@gmail.com>
2023-04-12 01:46:51 +0900

    오류수정

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,7 @@ if DEBUG:
     }
 else:
     DATABASES = {
-        "default": dj_database_url.config(
+        'default': dj_database_url.config(
             conn_max_age=600,
         )
     }
@@ -162,7 +162,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
